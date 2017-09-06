cb6188246f5ca4aed05c6062a2800cb340fdfb44
lbenychou <lbenychou@egencia.com>
2017-09-06 13:36:29 +0200

    real time trains

--- sandbox/multipleStations.js
+++ sandbox/multipleStations.js
@@ -2,7 +2,7 @@ import webservice from '../src/core/webservice'
 import somePlaces from '../src/core/somePlaces'
 import coloredStringifiedJson from './coloredStringifiedJson'
 
-webservice.nextDepartures(somePlaces.parisGareDeLyon, process.env.TOKEN)
+webservice.nextDepartures(somePlaces.laDefense, process.env.TOKEN)
     .then(data => coloredStringifiedJson(data))
     .then((highlightedData) => console.log(highlightedData))
 
--- src/core/webservice.js
+++ src/core/webservice.js
@@ -26,6 +26,7 @@ const haversine = (coords1, coords2) => {
 }
 
 const maxDistanceInRealTimeMap = 50000
+const thresholdBetweenTimeAndDistance = 5 //minutes before departure
 const realtimeMap = ({lat, long}) => get(`http://sncf-maps.hafas.de/carto/livemaps?service=journeygeopos&rect=${Math.floor(long * 1E6) - maxDistanceInRealTimeMap},${Math.floor(lat * 1E6) - maxDistanceInRealTimeMap},${Math.floor(long * 1E6) + maxDistanceInRealTimeMap},${Math.floor(lat * 1E6) + maxDistanceInRealTimeMap}&i=35000&is=10000&prod=27&date=${moment().format('YYYYMMDD')}&time=${moment().format('HHmm00')}&tpm=REPORT_ONLY&its=CT|INTERNATIONAL,CT|TGV,CT|INTERCITE,CT|TER,CT|TRANSILIEN&un=true&livemapCallback=`, {headers:{Referer:'http://www.sncf.com/fr/geolocalisation'}})
     .then(({data:{svcResL:[{res:{common:{prodL,remL,locL},jnyL}}]}}) => {
         const data = jnyL
@@ -179,13 +180,14 @@ export default {
         notify({timetable:{station: stationName, departures: departuresV3.map(x => x.departureData)}})
 
         const realTimeData = await realtimeMap(stationCoords)
-
         const departuresV4 = departuresV3.map(departure => {
             const realTimeTrain = realTimeData.filter(train => train.number === departure.savedNumber) [0]
+            const minutesBeforeDeparture = moment(departure.departureData.time, 'HH:mm').diff(moment(), 'minutes')
             return {...departure,
                 departureData: {
                     ...departure.departureData,
-                    time: realTimeTrain && realTimeTrain.names.includes('OnPlatform') ? 'à quai' :
+                    time: minutesBeforeDeparture > thresholdBetweenTimeAndDistance ? departure.departureData.time :
+                          realTimeTrain && realTimeTrain.names.includes('OnPlatform') ? 'à quai' :
                             realTimeTrain ? `< ${Math.ceil(realTimeTrain.distance)} km` : departure.departureData.time
                 }
             }
