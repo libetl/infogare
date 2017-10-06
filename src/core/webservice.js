805144a90bb8e0a32bb54774a0a91293c181efe1
libetl <libe4@free.fr>
2017-10-06 11:39:16 +0200

    typo

--- src/core/webservice.js
+++ src/core/webservice.js
@@ -6,19 +6,19 @@ export default {
     test: sources.sncfApi.testApi,
     suggestStations: (text) => sources.inMemory.stationsMatching(text),
     nextDepartures: async ({long, lat}, {token, notify = () => {}, mode = token ? 'sncfApi' : 'raildar'} = {}) => {
-        const preferedSource = sources[mode]
+        const preferredSource = sources[mode]
         const guessedStations = sources.inMemory.closestStations({long, lat})
         const guessedStationName = guessedStations[0].fields.intitule_gare
         const guessedStationCoords = {long: guessedStations[0].geometry.coordinates[0], lat: guessedStations[0].geometry.coordinates[1]}
 
         notify({timetable:{station: `${mode === 'raildar' ? 'recherche raildar...' : guessedStationName}\n(mise à jour...)`, departures: new Array(10).fill({})}})
 
-        const stationsAreas = await preferedSource.stationSearch(guessedStationCoords, {guessedStationName, token})
+        const stationsAreas = await preferredSource.stationSearch(guessedStationCoords, {guessedStationName, token})
         const stationName = stationsAreas[0].name_gare || guessedStationName
         const stationCoords = guessedStationCoords || {lat: stationsAreas[0].lat, long: stationsAreas[0].lng}
         const iataCodes = (guessedStations || sources.inMemory.closestStations(stationCoords)).map(station => (station.fields.tvs || '').split('|')[0])
 
-        const baseDepartures = removeDuplicates(sortByTime(await preferedSource.baseDepartures(stationsAreas, token)))
+        const baseDepartures = removeDuplicates(sortByTime(await preferredSource.baseDepartures(stationsAreas, token)))
 
         notify({timetable:{station: `${stationName}\n(mise à jour...)`, departures: baseDepartures.map(x => x.dataToDisplay)}})
 
