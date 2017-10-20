46f8053901b933895ea070aecc9adafc73f9b9f3
lbenychou <lbenychou@egencia.com>
2017-10-20 13:36:41 +0200

    removed old tests

--- sandbox/terSncfTest.js
+++
@@ -1,7 +0,0 @@
-import coloredStringifiedJson from './coloredStringifiedJson'
-import webservice from '../src/core/webservice'
-import places from '../src/core/data/places'
-
-webservice.nextDepartures(places.sainttienneBellevue, {mode: 'terSncf'})
-    .then(data => coloredStringifiedJson(data))
-    .then((highlightedData) => console.log(highlightedData))
