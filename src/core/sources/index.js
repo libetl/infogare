85b978c59a07c4f5002da8d74a07bbc2fc4832aa
lbenychou <lbenychou@egencia.com>
2017-10-31 15:49:46 +0100

    vianavigo, still working on

--- src/core/sources/index.js
+++ src/core/sources/index.js
@@ -5,6 +5,6 @@ import liveMap from './liveMap'
 import raildar from './raildar'
 import sncfApi from './sncfApi'
 import terSncf from './terSncf'
-import viaNavigo from './viaNavigo'
+import vianavigo from './vianavigo'
 
-export default { garesSncf, horairesInfoTrafic, inMemory, liveMap, raildar, sncfApi, terSncf, viaNavigo }
+export default { garesSncf, horairesInfoTrafic, inMemory, liveMap, raildar, sncfApi, terSncf, vianavigo }
