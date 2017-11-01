d7a0a3cddc68f1953ca461481aacddeeea4180e4
libetl <libe4@free.fr>
2017-11-01 17:02:25 +0100

    mode icon not enough visible

--- src/components/departure.js
+++ src/components/departure.js
@@ -22,7 +22,8 @@ export default class Departure extends React.Component {
             fontWeight: 'bold', borderStyle: 'solid', borderWidth: 3, borderRadius: mode === 'rer' ? 30 : 3,
             borderColor: `#${departure.color || 'FFFFFF'}`, color:`#${departure.color || 'FFFFFF'}`,
             marginTop: this.props.detailed ? 0 : 7}
-        const modeIcon = this.props.detailed ? {height: '23%', width:'10%'} : {marginTop: 7, height: '45%', width:'10%'}
+        const modeIcon = this.props.detailed ? {height: '23%', width:'10%', minHeight:this.props.rowHeight * 0.3, minWidth:this.props.rowHeight * 0.1} :
+            {marginTop: 7, height: '45%', width:'10%', minHeight:this.props.rowHeight * 0.5, minWidth:this.props.rowHeight * 0.1}
         const bigModeIcon = this.props.detailed ? {height: '23%', width:'22%'} : {marginTop: 7, height: '45%', width:'22%'}
         const modeText = this.props.detailed ?
             {color: '#fff', minWidth: this.props.rowWidth * 0.25, fontSize: Math.min(this.props.rowHeight * 0.45, this.props.rowWidth  * 0.20 / mode.length), fontWeight: 'bold', width: 60} :
@@ -31,9 +32,9 @@ export default class Departure extends React.Component {
         const oneStop = IsNative ? {color: '#fff', fontSize: stopsFontSize, lineHeight, includeFontPadding:false} : {color: '#fff', fontSize: stopsFontSize, lineHeight}
         const direction = {color: '#fff', fontSize: directionFontSize, overflow: 'hidden', flexGrow: 1}
         const split = {marginBottom: -lineHeight, height: '100%', width: '100%', flexDirection: 'row'}
-        const numberText = mode !== 'bus' ? {alignSelf: 'center'} :
+        const numberText = mode !== 'bus' ? {alignSelf: 'center', color:'#fff'} :
             {color: departure.fontColor || '#fff', backgroundColor: departure.color || 'transparent', minWidth:numberFontSize * 3, alignSelf: 'center', textAlign: 'center', fontSize: numberFontSize}
-        const number = {width: '15%', fontSize: numberFontSize}
+        const number = {width: '15%'}
         const timeAndStatus = {width: '20%', flexDirection: 'column'}
         const time = {color: '#dfc81f', fontSize: numberFontSize, fontWeight: 'bold'}
         const status = {color: '#f5a665', fontSize: numberFontSize, fontWeight: 'bold'}
@@ -44,11 +45,12 @@ export default class Departure extends React.Component {
                 <TouchableHighlight style={{width: '100%', height:'100%'}} onPress={() => this.props.parent.viewOneDeparture(this.props.num)} underlayColor='white'>
                     <View>
                         <View style={this.props.detailed ? split : styles.dontsplit} >
-                            {mode === 'transilien' ? <Image style={modeIcon} source={LoadPicture('transilien')} /> :
-                                mode === 'rer' ? <Image style={modeIcon} source={LoadPicture('rer')} /> :
-                                    mode === 'intercités' ? <Image style={bigModeIcon} source={LoadPicture('intercites')} /> :
-                                        mode === 'intercités de nuit' ? <Image style={bigModeIcon} source={LoadPicture('intercites')} /> :
-                                            <Text style={modeText}>{mode.toUpperCase()}</Text>}
+                            {mode === 'bus' ? <Image resizeMode='cover' style={modeIcon} source={LoadPicture('bus')} /> :
+                                mode === 'transilien' ? <Image resizeMode='cover' style={modeIcon} source={LoadPicture('transilien')} /> :
+                                    mode === 'rer' ? <Image resizeMode='cover' style={modeIcon} source={LoadPicture('rer')} /> :
+                                        mode === 'intercités' ? <Image resizeMode='cover' style={bigModeIcon} source={LoadPicture('intercites')} /> :
+                                            mode === 'intercités de nuit' ? <Image resizeMode='cover' style={bigModeIcon} source={LoadPicture('intercites')} /> :
+                                                <Text style={modeText}>{mode.toUpperCase()}</Text>}
                             {departure.name ? <Text style={lineColorStyle}>{departure.name}</Text> : <Text>{departure.name}</Text>}
                             <View style={number}><Text style={numberText}>{departure.number}</Text></View>
                             <View style={timeAndStatus}><Text style={time}>{departure.time}</Text>{departure.status ? (<Text style={status}>{departure.status}</Text>) : <Text/>}</View>
