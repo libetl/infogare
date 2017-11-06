e5d93ddeebbec8f7e311f929309c9305a8d7191e
lbenychou <lbenychou@egencia.com>
2017-11-06 13:27:52 +0100

    more tolerant transilien data source

--- sandbox/testTransilien.js
+++ sandbox/testTransilien.js
@@ -2,5 +2,5 @@ import webservice from '../src/core/webservice'
 import places from '../src/core/data/places'
 import coloredStringifiedJson from './coloredStringifiedJson'
 
-webservice.nextDepartures(places.laDefense, { dataSourceByFeature:{stations: 'inMemory', departures: 'transilien'}}).then(data => coloredStringifiedJson(data))
+webservice.nextDepartures(places.creil, { dataSourceByFeature:{stations: 'inMemory', departures: 'transilien'}}).then(data => coloredStringifiedJson(data))
     .then(highlightedData => console.log(highlightedData))
--- src/core/sources/transilien.js
+++ src/core/sources/transilien.js
@@ -13,8 +13,24 @@ const transilien = html => new DomParser().parseFromString(html.data)
     .map(result=>result[5] === 'Voie --' ? [...result.slice(0, 5), '', ...result.slice(5)] : result)
     .map(result=>[result[0].toUpperCase(), result[1].substring(result[1].indexOf('-')+1).toUpperCase(), ...result.slice(2)])
     .map((result,i)=> {
-        const hour = result[3]
         const now = moment().format('HH:mm')
+        if (result[1].match(/^[0-9]{2}:[0-9]{2}$/)) {
+            const hour = result[1]
+            return {
+                savedNumber: result[0] + i,
+                stop_date_time: {
+                    base_departure_date_time: hour.localeCompare(now) < 0 ? `${parseInt(hour.split(':')[0]) + 24}:${hour.split(':')[1]}` : hour,
+                },
+                dataToDisplay: {
+                    platform: result[2].match(/^[0-9]*$/) ? result[2] : result[3],
+                    direction: capitalize(result[2].match(/^[0-9]*$/) ? 'Inconnue' : result[2]),
+                    time: result[1],
+                    missionCode: result[0],
+                    stops: []
+                }
+            }
+        }
+        const hour = result[3]
         return {
             savedNumber:result[2]+i,
             stop_date_time: {
