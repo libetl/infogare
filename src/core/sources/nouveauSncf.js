52d2c6850e1622c4c0ecbd1d413d7b07ace6526c
libetl <libe4@free.fr>
2017-11-12 22:38:04 +0100

    partial stops list in nouveauSncf instead of full stops list

--- src/core/sources/nouveauSncf.js
+++ src/core/sources/nouveauSncf.js
@@ -11,18 +11,21 @@ const getStation = stationName => get('https://www.nouveau.sncf.com/api/iv/1.0/u
         indicateurReponseGaresSecondaires: true}})
 
 const extractstopAreaName = response =>
-    response.data.reponseRechercherListeEmplacementsCommencantPar.reponse.listeResultats.resultat[0].donnees.listeEmplacements.emplacement[0].code
+    response.data.reponseRechercherListeEmplacementsCommencantPar.reponse.listeResultats.resultat[0].donnees.listeEmplacements.emplacement[0]
 const extractDepartures = response => response.data.reponseRechercherProchainsDeparts.reponse.listeResultats.resultat.reduce((acc, resultat) => acc.concat(resultat.donnees.listeHoraires.horaire),[])
 const extractJourney = response => (response.data.reponseRechercherListeCirculations.reponse.listeResultats.resultat[0].donnees.listeCirculations||{circulation:[{listeArretsDesserte:{arret:[{emplacement:{libelle:'Desserte non dispo'}}]}}]}).circulation[0]
 
 const departures = stopAreaName => get(`https://www.nouveau.sncf.com/api/iv/1.0/infoVoy/rechercherProchainsDeparts?codeZoneArret=${stopAreaName}&indicateurReponseGaresSecondaires=true`)
-const findJourney = ({baseDepartures, stationsAreas:{apiData:{stopAreaName}}}) =>
+const findJourney = ({baseDepartures, stationsAreas:{apiData:{name, stopAreaName}}}) =>
     Promise.all(
         baseDepartures.slice(0, 2).map(departure =>
             get(`https://www.nouveau.sncf.com/api/iv/1.0/infoVoy/rechercherListeCirculations?numero=${departure.savedNumber}&dateCirculation=${moment(departure.stop_date_time.base_departure_date_time, 'YYYY-MM-DDTHH:mm:ssZ').format('YYYY-MM-DD')}&codeZoneArret=${stopAreaName}&typeHoraire=TEMPS_REEL&codeZoneArretDepart&codeZoneArretArrivee&compositions=1&codeCirculation`)
                 .then(response => extractJourney(response))
                 .then(journey => journey.listeArretsDesserte.arret.map(arret => arret.emplacement.libelle))
-                .then(stops => {return {savedNumber:departure.savedNumber, dataToDisplay:{stops}}})
+                .then((allStops = []) => {
+                    const index = allStops.indexOf(name)
+                    const stops = allStops.slice(index === -1 ? 0 : index + 1)
+                    return {savedNumber:departure.savedNumber, dataToDisplay:{stops}}})
                 .catch(e => ['Desserte non dispo'])))
 
 const baseDepartures = ({apiData}) =>
@@ -46,7 +49,8 @@ const stationSearch = (coords, {token, nestedStationSearch}) => {
     const stationsAreas = nestedStationSearch(coords, {token})
     const {stationCoords, stationName, stations} = stationsAreas['nestedSearchData']
     return getStation(stationName).then(response => extractstopAreaName(response))
-        .then(stopAreaName => {return {apiData:{stopAreaName}, stations, ...stationsAreas, stationCoords, stationName:stationsAreas.stationName}})
+        .then(stopArea => {return {apiData:{stopAreaName:stopArea.code, name:stopArea.libelle}, 
+            stations, ...stationsAreas, stationCoords, stationName:stationsAreas.stationName}})
 }
 
 export default {
