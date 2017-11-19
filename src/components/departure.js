e5bc3c8adad71598624cd649f79b9ef300b14a10
libetl <libe4@free.fr>
2017-11-19 14:22:49 +0100

    display fixes for vianavigo data source

--- src/components/departure.js
+++ src/components/departure.js
@@ -14,22 +14,23 @@ export default class Departure extends React.Component {
             departure.direction : departure.stops[departure.stops.length - 1]) || ' '
         const directionFontSize = Math.min(this.props.rowHeight * 0.3, this.props.rowWidth * 0.8 / directionName.length)
         const numberFontSize = Math.min(this.props.rowHeight * 0.1, this.props.rowWidth / directionName.length)
+        const descriptionHeight = this.props.rowHeight * 0.3
         const style = {width: '100%', backgroundColor: this.props.odd ? '#0d5da6' : '#04396d', flexDirection: 'column',
             alignItems: 'center', flexWrap: 'wrap', height: `${70.0 / this.props.rows * (this.props.detailed ? 2 : 1)}%`}
         const mode = (departure.mode || '').toLowerCase()
         const lineColor = {width: '10%', height:this.props.detailed ? '45%' : '62%'}
-        const lineColorText = {width:numberFontSize * 3, height:numberFontSize * 3,
-            paddingTop:(this.props.rowHeight * 0.23  - numberFontSize) / 2,
+        const lineColorText = {width:descriptionHeight, height:descriptionHeight,
+            paddingTop: mode === 'metro' ? 0 : (this.props.rowHeight * 0.23  - numberFontSize) / 2,
             paddingBottom:(this.props.rowHeight * 0.23  - numberFontSize) / 2,
-            fontSize:this.props.rowHeight * 0.11,
+            fontSize: mode === 'metro' ? this.props.rowHeight * 0.2 : this.props.rowHeight * 0.11,
             textAlign: 'center', fontWeight: 'bold', borderStyle: 'solid', borderWidth: 3,
             borderRadius: mode === 'rer' || mode === 'metro' || mode === 'tramway' ? (IsNative ? 50 : '50%') : 3,
             borderColor: `#${departure.color || 'FFFFFF'}`,
             color:mode === 'metro' && departure.fontColor ? `#${departure.fontColor}` : `#${departure.color || 'FFFFFF'}`,
             backgroundColor: mode === 'metro' ? `#${departure.color}` : 'transparent'}
         const modeView = {width:'10%'}
-        const modeIcon = {width: numberFontSize * 3, height: numberFontSize * 3, alignSelf: 'center'}
-        const bigModeIcon = {height: numberFontSize * 3}
+        const modeIcon = {width: descriptionHeight, height: descriptionHeight, alignSelf: 'center'}
+        const bigModeIcon = {height: descriptionHeight}
         const modeText = {color: '#fff', maxWidth: this.props.rowWidth * 0.25, fontSize: numberFontSize, fontWeight: 'bold', width: 60}
         const direction = {color: '#fff', fontSize: directionFontSize, overflow: 'hidden', flexGrow: 1}
         const split = {height:this.props.rowHeight * 0.3, width: '100%', flexDirection: 'row'}
@@ -38,7 +39,7 @@ export default class Departure extends React.Component {
             {color: departure.fontColor ? '#' + departure.fontColor : '#fff', backgroundColor: departure.color ? '#' + departure.color : 'transparent', minWidth:numberFontSize * 3, alignSelf: 'center', textAlign: 'center', fontSize: numberFontSize}
         const number = {width: this.props.mustBePadded ? '25%' : '15%'}
         const timeAndStatus = {minWidth:3 * numberFontSize + 4, flexDirection: 'column'}
-        const time = {color: '#dfc81f', fontSize: numberFontSize, fontWeight: 'bold'}
+        const time = {color: '#dfc81f', fontSize: descriptionHeight / 3, fontWeight: 'bold'}
         const status = {color: '#f5a665', fontSize: numberFontSize, fontWeight: 'bold'}
         const platform = {color: '#fff', borderStyle: 'solid', borderWidth: 1, borderColor: '#fff', minWidth: 30, minHeight: 30, borderRadius: 6, width: numberFontSize, height: numberFontSize, textAlign: 'center', flexShrink: 1}
         const androidSpecialAttributes = IsNative ? {renderToHardwareTextureAndroid: true} : {}
