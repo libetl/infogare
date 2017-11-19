cc94ef9f0b1b45c2c246940c5d658baf88a91f98
libetl <libe4@free.fr>
2017-11-19 14:46:50 +0100

    trying more coordinates with vianavigo

--- src/core/sources/vianavigo.js
+++ src/core/sources/vianavigo.js
@@ -5,7 +5,7 @@ import promiseWhile from '../operations/promiseWhile'
 import capitalize from '../operations/capitalize'
 
 const epsg102582 = '+proj=lcc +lat_1=46.8 +lat_0=46.8 +lon_0=0 +k_0=0.99987742 +x_0=600000 +y_0=2200000 +a=6378249.2 +b=6356515 +towgs84=-168,-60,320,0,0,0,0 +pm=paris +units=m +no_defs'
-const lookAround = [[0, 0],[-35, -35], [-35, 35], [35, -35], [35, 35]]
+const lookAround = [[0, 0],[-35, -35], [-35, 35], [35, -35], [35, 35],[-70, -70], [-70, 70], [70, -70], [70, 70]]
 
 const stationSearch = (coords, {nestedStationSearch}) => Promise.resolve({coords, projection:proj4(epsg102582, [coords.long, coords.lat]), inMemoryData:nestedStationSearch(coords), lookAroundIndex: 0})
     .then(({coords, projection, inMemoryData, lookAroundIndex}) =>
